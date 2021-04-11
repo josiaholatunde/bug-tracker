bef5fb7352f1a283c23ef94074ecedf01ae08410
Ogunboyejo Olatunde <oogunnboyejo@teamapt.com>
2021-04-11 02:50:55 +0100

    fix: FIxed bug with retrieving applications

--- src/main/java/com/example/fundamentals/dtos/ApplicationDto.java
+++ src/main/java/com/example/fundamentals/dtos/ApplicationDto.java
@@ -3,11 +3,13 @@ package com.example.fundamentals.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApplicationDto {
